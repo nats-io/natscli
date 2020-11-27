6d5bd2d9b7623cda3e744667bf6aa1a5d9dc9f9e
R.I.Pienaar <rip@devco.net>
2020-11-27 15:07:15 +0100

    support publishing multiple messages
    Signed-off-by: R.I.Pienaar <rip@devco.net>

--- nats/pub_command.go
+++ nats/pub_command.go
@@ -14,11 +14,13 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 	"strings"
+	"text/template"
 	"time"
 
 	"github.com/nats-io/nats.go"
@@ -33,16 +35,35 @@ type pubCmd struct {
 	replyTo string
 	raw     bool
 	hdrs    []string
+	cnt     int
 }
 
 func configurePubCommand(app *kingpin.Application) {
 	c := &pubCmd{}
-	pub := app.Command("pub", "Generic data publishing utility").Action(c.publish)
+	help := `Generic data publishing utility
+
+When publishing multiple messages using the "count" flag
+the body of the messages may use Go templates to create
+multiple unique messages.
+
+   nats pub test --count 10 "Message {{.Cnt}} @ {{.Time}}"
+
+Available template variables are:
+
+   .Cnt       the message number
+   .TimeStamp RFC3339 format current time
+   .Unix      seconds since 1970 in UTC
+   .UnixNano  nano seconds since 1970 in UTC
+   .Time      the current time
+
+`
+	pub := app.Command("pub", help).Action(c.publish)
 	pub.Arg("subject", "Subject to subscribe to").Required().StringVar(&c.subject)
 	pub.Arg("body", "Message body").Default("!nil!").StringVar(&c.body)
 	pub.Flag("wait", "Wait for a reply from a service").Short('w').BoolVar(&c.req)
 	pub.Flag("reply", "Sets a custom reply to subject").StringVar(&c.replyTo)
 	pub.Flag("header", "Adds headers to the message").Short('H').StringsVar(&c.hdrs)
+	pub.Flag("count", "Publish multiple messages").IntVar(&c.cnt)
 
 	req := app.Command("request", "Generic data request utility").Alias("req").Action(c.publish)
 	req.Arg("subject", "Subject to subscribe to").Required().StringVar(&c.subject)
@@ -52,12 +73,53 @@ func configurePubCommand(app *kingpin.Application) {
 	req.Flag("header", "Adds headers to the message").Short('H').StringsVar(&c.hdrs)
 }
 
-func (c *pubCmd) prepareMsg() (*nats.Msg, error) {
+func (c *pubCmd) prepareMsg(body []byte) (*nats.Msg, error) {
 	msg := nats.NewMsg(c.subject)
 	msg.Reply = c.replyTo
-	msg.Data = []byte(c.body)
+	msg.Data = body
 
 	return msg, parseStringsToHeader(c.hdrs, msg)
+}
+
+func (c *pubCmd) doReq(nc *nats.Conn) error {
+	start := time.Now()
+	if !c.raw {
+		log.Printf("Sending request on %q\n", c.subject)
+	}
+
+	msg, err := c.prepareMsg([]byte(c.body))
+	if err != nil {
+		return err
+	}
+
+	m, err := nc.RequestMsg(msg, timeout)
+	if err != nil {
+		return err
+	}
+
+	if c.raw {
+		fmt.Println(string(m.Data))
+
+		return nil
+	}
+
+	log.Printf("Received on %q rtt %v", m.Subject, time.Since(start))
+	if len(m.Header) > 0 {
+		for h, vals := range m.Header {
+			for _, val := range vals {
+				log.Printf("%s: %s", h, val)
+			}
+		}
+
+		fmt.Println()
+	}
+
+	fmt.Println(string(m.Data))
+	if !strings.HasSuffix(string(m.Data), "\n") {
+		fmt.Println()
+	}
+
+	return nil
 }
 
 func (c *pubCmd) publish(_ *kingpin.ParseContext) error {
@@ -77,63 +139,55 @@ func (c *pubCmd) publish(_ *kingpin.ParseContext) error {
 	}
 
 	if c.req {
-		start := time.Now()
-		if !c.raw {
-			log.Printf("Sending request on %q\n", c.subject)
-		}
+		return c.doReq(nc)
+	}
 
-		msg, err := c.prepareMsg()
+	type pubData struct {
+		Cnt       int
+		Unix      int64
+		UnixNano  int64
+		TimeStamp string
+		Time      string
+	}
+
+	t, err := template.New("body").Parse(c.body)
+	if err != nil {
+		return err
+	}
+
+	for i := 0; i < c.cnt; i++ {
+		var body bytes.Buffer
+		now := time.Now()
+		err = t.Execute(&body, &pubData{
+			Cnt:       i,
+			Unix:      now.Unix(),
+			UnixNano:  now.UnixNano(),
+			TimeStamp: now.Format(time.RFC3339),
+			Time:      now.Format(time.Kitchen),
+		})
 		if err != nil {
 			return err
 		}
 
-		m, err := nc.RequestMsg(msg, timeout)
+		msg, err := c.prepareMsg(body.Bytes())
 		if err != nil {
 			return err
 		}
 
-		if c.raw {
-			fmt.Println(string(m.Data))
+		err = nc.PublishMsg(msg)
+		if err != nil {
+			return err
+		}
+		nc.Flush()
 
-			return nil
+		err = nc.LastError()
+		if err != nil {
+			return err
 		}
 
-		log.Printf("Received on %q rtt %v", m.Subject, time.Since(start))
-		if len(m.Header) > 0 {
-			for h, vals := range m.Header {
-				for _, val := range vals {
-					log.Printf("%s: %s", h, val)
-				}
-			}
-
-			fmt.Println()
-		}
-
-		fmt.Println(string(m.Data))
-		if !strings.HasSuffix(string(m.Data), "\n") {
-			fmt.Println()
-		}
-
-		return nil
+		log.Printf("Published %d bytes to %q\n", len(c.body), c.subject)
 	}
-
-	msg, err := c.prepareMsg()
-	if err != nil {
-		return err
-	}
-
-	err = nc.PublishMsg(msg)
-	if err != nil {
-		return err
-	}
-	nc.Flush()
-
-	err = nc.LastError()
-	if err != nil {
-		return err
-	}
-
-	log.Printf("Published %d bytes to %q\n", len(c.body), c.subject)
 
 	return nil
+
 }
