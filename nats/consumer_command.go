9fee8e6f18fad786bfece2e1170afc252ae9e6bf
Brad Midgley <brad.midgley@spiff.com>
2021-04-23 16:42:58 -0600

    Add --max-pending suggestion to help

--- nats/consumer_command.go
+++ nats/consumer_command.go
@@ -672,7 +672,7 @@ func (c *consumerCmd) prepareConfig(pc *kingpin.ParseContext) (cfg *api.Consumer
 		err = survey.AskOne(&survey.Input{
 			Message: "Maximum Acknowledgements Pending",
 			Default: "0",
-			Help:    "The maximum number of messages without acknowledgement that can be outstanding, once this limit is reached message delivery will be suspended",
+			Help:    "The maximum number of messages without acknowledgement that can be outstanding, once this limit is reached message delivery will be suspended. Settable using --max-pending.",
 		}, &c.maxAckPending)
 		kingpin.FatalIfError(err, "could not ask for maximum outstanding acknowledgements")
 	}
