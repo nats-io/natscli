e2136ffa4836a22042b82d3342152007a210cc98
R.I.Pienaar <rip@devco.net>
2022-02-25 10:51:28 +0100

    add stream and consumer cheats
    Signed-off-by: R.I.Pienaar <rip@devco.net>

--- cli/consumer_command.go
+++ cli/consumer_command.go
@@ -185,6 +185,23 @@ func configureConsumerCommand(app commandHost) {
 	conReport.Arg("stream", "Stream name").StringVar(&c.stream)
 	conReport.Flag("raw", "Show un-formatted numbers").Short('r').BoolVar(&c.raw)
 	conReport.Flag("leaders", "Show details about the leaders").Short('l').BoolVar(&c.reportLeaderDistrib)
+
+	cheats["consumer"] = `# Adding, Removing, Viewing a Consumer
+nats consumer add
+nats consumer info ORDERS NEW
+nats consumer rm ORDERS NEW
+
+# Editing a consumer
+nats consumer edit ORDERS NEW --description "new description"
+
+# Get messages from a consumer
+nats consumer next ORDERS NEW --ack
+nats consumer next ORDERS NEW --no-ack
+nats consumer sub ORDERS NEW --ack
+
+# Force leader election on a consumer
+nats consumer cluster down ORDERS NEW
+`
 }
 
 func init() {
