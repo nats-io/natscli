9d9fababd53ab23ff39776681e5da2f89822be25
D. Sidiropoulos <dominick.sidiropoulos@gmail.com>
2022-05-16 14:17:27 +0200

    fix (main.go): simplify the cli-flag definitions for 'js-domain' and 'domain' by removing superfluous calls to .Placeholder()

--- nats/main.go
+++ nats/main.go
@@ -55,9 +55,9 @@ See 'nats cheat' for a quick cheatsheet of commands
 	ncli.Flag("timeout", "Time to wait on responses from NATS").Default("5s").Envar("NATS_TIMEOUT").PlaceHolder("NATS_TIMEOUT").DurationVar(&opts.Timeout)
 	ncli.Flag("js-api-prefix", "Subject prefix for access to JetStream API").PlaceHolder("PREFIX").StringVar(&opts.JsApiPrefix)
 	ncli.Flag("js-event-prefix", "Subject prefix for access to JetStream Advisories").PlaceHolder("PREFIX").StringVar(&opts.JsEventPrefix)
-	ncli.Flag("js-domain", "JetStream domain to access").PlaceHolder("PREFIX").PlaceHolder("DOMAIN").StringVar(&opts.JsDomain)
+	ncli.Flag("js-domain", "JetStream domain to access").PlaceHolder("DOMAIN").StringVar(&opts.JsDomain)
 	ncli.Flag("inbox-prefix", "Custom inbox prefix to use for inboxes").PlaceHolder("PREFIX").StringVar(&opts.InboxPrefix)
-	ncli.Flag("domain", "JetStream domain to access").PlaceHolder("PREFIX").PlaceHolder("DOMAIN").Hidden().StringVar(&opts.JsDomain)
+	ncli.Flag("domain", "JetStream domain to access").PlaceHolder("DOMAIN").Hidden().StringVar(&opts.JsDomain)
 	ncli.Flag("context", "Configuration context").Envar("NATS_CONTEXT").StringVar(&opts.CfgCtx)
 	ncli.Flag("trace", "Trace API interactions").BoolVar(&opts.Trace)
 
