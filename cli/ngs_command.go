1a0a954110fd8153b6e8605049b8adef91d2f672
Boris Ilijic <boris.ilijic@gmail.com>
2023-11-12 19:10:53 +0100

    Add full ngs rtt command implementation

--- cli/ngs_command.go
+++ cli/ngs_command.go
@@ -9,6 +9,21 @@ import (
 
 type ngsCmd struct {
 	rttCmd rttCmd
+
+	cloudAWS bool
+	cloudAZ  bool
+	cloudGCP bool
+	cloudAll bool
+
+	regionalAsia   bool
+	regionalEU     bool
+	regionalUS     bool
+	regionalUSWest bool
+	regionalUSEast bool
+	regionalAll    bool
+
+	global bool
+	all    bool
 }
 
 func configureNgsCommand(app commandHost) {
@@ -19,33 +34,66 @@ func configureNgsCommand(app commandHost) {
 	ngs := app.Command("ngs", "NGS helpers")
 	ngs.HelpLong("WARNING: This command is experimental")
 
-	rtt := ngs.Command("rtt", "Displays RTT for all NGS endpoints").Action(c.rttHandler)
-	rtt.Arg("iterations", "How many round trips to do when testing").Default("5").IntVar(&c.rttCmd.iterations)
+	rtt := ngs.Command("rtt", "Displays RTT for all NGS endpoints. See: https://docs.synadia.com/ngs/resources/connection-endpoints").Action(c.rttHandler)
+	rtt.Arg("iterations", "How many round trips to do when testing each NGS endpoint").Default("3").IntVar(&c.rttCmd.iterations)
+	rtt.Flag("global", fmt.Sprintf("query default (global) endpoint: %s", globalEp)).Default("true").Short('g').UnNegatableBoolVar(&c.global)
+	rtt.Flag("all-clouds", "query all Cloud Endpoints (AWS, AZ, GCP)").Short('c').UnNegatableBoolVar(&c.cloudAll)
+	rtt.Flag("aws", fmt.Sprintf("query AWS Cloud Endpoint: %s", cloudAWSEp)).UnNegatableBoolVar(&c.cloudAWS)
+	rtt.Flag("az", fmt.Sprintf("query AZ Cloud Endpoint: %s", cloudAZEp)).UnNegatableBoolVar(&c.cloudAZ)
+	rtt.Flag("gpc", fmt.Sprintf("query GCP Cloud Endpoint: %s", cloudGCPEp)).UnNegatableBoolVar(&c.cloudGCP)
+	rtt.Flag("all-regions", "query all Regional Endpoints (Asia, EU, US)").Short('r').UnNegatableBoolVar(&c.regionalAll)
+	rtt.Flag("asia", fmt.Sprintf("query regional endpoint in Asia: %s", regionalAsia)).UnNegatableBoolVar(&c.regionalAsia)
+	rtt.Flag("eu", fmt.Sprintf("query regional endpoint in EU: %s", regionalEU)).UnNegatableBoolVar(&c.regionalEU)
+	rtt.Flag("us", fmt.Sprintf("query regional endpoint in US: %s", regionalUS)).UnNegatableBoolVar(&c.regionalUS)
+	rtt.Flag("us-east", fmt.Sprintf("query regional endpoint in US East: %s", regionalUSEast)).UnNegatableBoolVar(&c.regionalUSEast)
+	rtt.Flag("us-west", fmt.Sprintf("query regional endpoint in US West: %s", regionalUSWest)).UnNegatableBoolVar(&c.regionalUSWest)
+	rtt.Flag("all", "query all available endpoints").Short('a').UnNegatableBoolVar(&c.all)
 }
 
 func init() {
 	registerCommand("ngs", 0, configureNgsCommand)
 }
 
+const (
+	globalEp       = "tls://connect.ngs.global"
+	cloudAWSEp     = "tls://aws.cloud.ngs.global"
+	cloudAZEp      = "tls://az.cloud.ngs.global"
+	cloudGCPEp     = "tls://gcp.cloud.ngs.global"
+	regionalAsia   = "tls://asia.geo.ngs.global"
+	regionalEU     = "tls://eu.geo.ngs.global"
+	regionalUS     = "tls://us.geo.ngs.global"
+	regionalUSWest = "tls://west.us.geo.ngs.global"
+	regionalUSEast = "tls://east.us.geo.ngs.global"
+)
+
 var (
-	endpoints = []string{
-		"tls://aws.cloud.ngs.global",
-		"tls://az.cloud.ngs.global",
-		"tls://gcp.cloud.ngs.global",
-		"tls://asia.geo.ngs.global",
-		"tls://eu.geo.ngs.global",
-		"tls://us.geo.ngs.global",
-		"tls://west.us.geo.ngs.global",
-		"tls://west.us.geo.ngs.global",
+	cloudEp = []string{
+		cloudAWSEp,
+		cloudAZEp,
+		cloudGCPEp,
+	}
+	reginalEp = []string{
+		regionalAsia,
+		regionalEU,
+		regionalUS,
+		regionalUSWest,
+		regionalUSEast,
+	}
+	allEp = []string{
+		globalEp,
+		cloudAWSEp,
+		cloudAZEp,
+		cloudGCPEp,
+		regionalAsia,
+		regionalEU,
+		regionalUS,
+		regionalUSWest,
+		regionalUSEast,
 	}
 )
 
 func (c *ngsCmd) rttHandler(_ *fisk.ParseContext) error {
-	servers := strings.Join(endpoints, ",")
-	fmt.Println("servers", servers)
-
-	table := newTableWriter("Cloud Provider Endpoints")
-	table.AddHeaders("Endpoint", "RTT")
+	servers := c.resolveServers()
 
 	targets, err := c.rttCmd.targets(servers)
 	if err != nil {
@@ -53,10 +101,12 @@ func (c *ngsCmd) rttHandler(_ *fisk.ParseContext) error {
 	}
 
 	err = c.rttCmd.performTest(targets)
-
 	if err != nil {
 		return err
 	}
+
+	table := newTableWriter("NGS Endpoints")
+	table.AddHeaders("Endpoint", "RTT")
 
 	for _, t := range targets {
 		rtts := ""
@@ -65,11 +115,60 @@ func (c *ngsCmd) rttHandler(_ *fisk.ParseContext) error {
 			rtts += fmt.Sprintf(f, rtt.Address, rtt.RTT)
 		}
 		table.AddRow(t.URL, rtts)
-		// if i != len(targets)-1 {
-		// 	table.AddSeparator()
-		// }
+		table.AddSeparator()
 	}
 
 	fmt.Println(table.Render())
 	return nil
+}
+
+func (c *ngsCmd) resolveServers() string {
+	fmtRes := func(res []string) string {
+		return strings.Join(res, ",")
+	}
+
+	if c.all {
+		return fmtRes(allEp)
+	}
+
+	servers := []string{}
+	if c.global {
+		servers = append(servers, globalEp)
+	}
+
+	if c.cloudAll {
+		servers = append(servers, cloudEp...)
+	} else {
+		if c.cloudAWS {
+			servers = append(servers, cloudAWSEp)
+		}
+		if c.cloudAZ {
+			servers = append(servers, cloudAZEp)
+		}
+		if c.cloudGCP {
+			servers = append(servers, cloudGCPEp)
+		}
+	}
+
+	if c.regionalAll {
+		servers = append(servers, reginalEp...)
+	} else {
+		if c.regionalAsia {
+			servers = append(servers, regionalAsia)
+		}
+		if c.regionalEU {
+			servers = append(servers, regionalEU)
+		}
+		if c.regionalUS {
+			servers = append(servers, regionalUS)
+		}
+		if c.regionalUSEast {
+			servers = append(servers, regionalUSEast)
+		}
+		if c.regionalUSWest {
+			servers = append(servers, regionalUSWest)
+		}
+	}
+
+	return fmtRes(servers)
 }
