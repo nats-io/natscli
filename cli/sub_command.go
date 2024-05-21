89762724043c0690627690bd5f2387b6bc61c5ae
Jean-Noël Moyne <jnmoyne@gmail.com>
2024-05-21 11:59:31 -0700

    Adds flags to `nats sub`
    - `--subjects-only` only prints the subject for each message received
    - `--timestamp` adds a time stamp of when the message is received
    - `--delta-time` adds a time since the start of the command of when the message is received
    
    Signed-off-by: Jean-Noël Moyne <jnmoyne@gmail.com>

--- cli/sub_command.go
+++ cli/sub_command.go
@@ -57,6 +57,9 @@ type subCmd struct {
 	jetStream             bool
 	ignoreSubjects        []string
 	wait                  time.Duration
+	ts                    bool
+	dt                    bool
+	sOnly                 bool
 }
 
 func configureSubCommand(app commandHost) {
@@ -86,6 +89,9 @@ func configureSubCommand(app commandHost) {
 	act.Flag("wait", "Unsubscribe after this amount of time without any traffic").DurationVar(&c.wait)
 	act.Flag("report-subjects", "Subscribes to a subject pattern and builds a de-duplicated report of active subjects receiving data").UnNegatableBoolVar(&c.reportSubjects)
 	act.Flag("report-top", "Number of subjects to show when doing 'report-subjects'. Default is 10.").Default("10").IntVar(&c.reportSubjectsCount)
+	act.Flag("timestamp", "Show timestamps in output").Short('t').UnNegatableBoolVar(&c.ts)
+	act.Flag("delta-time", "Show delta time in output").Short('d').UnNegatableBoolVar(&c.dt)
+	act.Flag("subjects-only", "Prints only the messages' subjects").UnNegatableBoolVar(&c.sOnly)
 }
 
 func init() {
@@ -183,6 +189,9 @@ func (c *subCmd) subscribe(p *fisk.ParseContext) error {
 	if c.reportSubjects && c.reportSubjectsCount == 0 {
 		return fmt.Errorf("subject count must be at least one")
 	}
+	if c.ts && c.dt {
+		return fmt.Errorf("timestamp and delta-time flags are mutually exclusive")
+	}
 
 	if c.dump != "" && c.dump != "-" {
 		err = os.MkdirAll(c.dump, 0700)
@@ -205,6 +214,8 @@ func (c *subCmd) subscribe(p *fisk.ParseContext) error {
 
 		subjectReportMap      map[string]int64
 		subjectBytesReportMap map[string]int64
+
+		startTime = time.Now()
 	)
 	defer cancel()
 
@@ -266,7 +277,7 @@ func (c *subCmd) subscribe(p *fisk.ParseContext) error {
 			if c.match && m.Reply != "" {
 				matchMap[m.Reply] = m
 			} else {
-				c.printMsg(m, nil, ctr)
+				c.printMsg(m, nil, ctr, startTime)
 			}
 		}
 
@@ -298,7 +309,7 @@ func (c *subCmd) subscribe(p *fisk.ParseContext) error {
 			return
 		}
 
-		c.printMsg(request, reply, ctr)
+		c.printMsg(request, reply, ctr, startTime)
 		delete(matchMap, reply.Subject)
 
 		// if reached limit and matched all requests
@@ -489,7 +500,7 @@ func (c *subCmd) firstSubject() string {
 	return c.subjects[0]
 }
 
-func (c *subCmd) printMsg(msg *nats.Msg, reply *nats.Msg, ctr uint) {
+func (c *subCmd) printMsg(msg *nats.Msg, reply *nats.Msg, ctr uint, startTime time.Time) {
 	var info *jsm.MsgInfo
 	if msg.Reply != "" {
 		info, _ = jsm.ParseJSMsgMetadata(msg)
@@ -497,6 +508,13 @@ func (c *subCmd) printMsg(msg *nats.Msg, reply *nats.Msg, ctr uint) {
 
 	if opts().Trace && msg.Reply != "" {
 		fmt.Printf("<<< Reply Subject: %v\n", msg.Reply)
+	}
+
+	var timeStamp string
+	if c.ts {
+		timeStamp = fmt.Sprintf(" @ %s", time.Now().Format(time.StampMicro))
+	} else if c.dt {
+		timeStamp = fmt.Sprintf(" @ %s", time.Since(startTime).String())
 	}
 
 	if c.dump != "" {
@@ -534,29 +552,31 @@ func (c *subCmd) printMsg(msg *nats.Msg, reply *nats.Msg, ctr uint) {
 
 		if info == nil {
 			if msg.Reply != "" {
-				fmt.Printf("[#%d] Received on %q with reply %q\n", ctr, msg.Subject, msg.Reply)
+				fmt.Printf("[#%d]%s Received on %q with reply %q\n", ctr, timeStamp, msg.Subject, msg.Reply)
 			} else {
-				fmt.Printf("[#%d] Received on %q\n", ctr, msg.Subject)
+				fmt.Printf("[#%d]%s Received on %q\n", ctr, timeStamp, msg.Subject)
 			}
 		} else if c.jetStream {
-			fmt.Printf("[#%d] Received JetStream message: stream: %s seq %d / subject: %s / time: %v\n", ctr, info.Stream(), info.StreamSequence(), msg.Subject, info.TimeStamp().Format(time.RFC3339))
+			fmt.Printf("[#%d]%s Received JetStream message: stream: %s seq %d / subject: %s / time: %v\n", ctr, timeStamp, info.Stream(), info.StreamSequence(), msg.Subject, info.TimeStamp().Format(time.RFC3339))
 		} else {
-			fmt.Printf("[#%d] Received JetStream message: consumer: %s > %s / subject: %s / delivered: %d / consumer seq: %d / stream seq: %d\n", ctr, info.Stream(), info.Consumer(), msg.Subject, info.Delivered(), info.ConsumerSequence(), info.StreamSequence())
+			fmt.Printf("[#%d]%s Received JetStream message: consumer: %s > %s / subject: %s / delivered: %d / consumer seq: %d / stream seq: %d\n", ctr, timeStamp, info.Stream(), info.Consumer(), msg.Subject, info.Delivered(), info.ConsumerSequence(), info.StreamSequence())
 		}
 
-		prettyPrintMsg(msg, c.headersOnly, c.translate)
+		if !c.sOnly {
+			prettyPrintMsg(msg, c.headersOnly, c.translate)
 
-		if reply != nil {
-			if info == nil {
-				fmt.Printf("[#%d] Matched reply on %q\n", ctr, reply.Subject)
-			} else if c.jetStream {
-				fmt.Printf("[#%d] Matched reply JetStream message: stream: %s seq %d / subject: %s / time: %v\n", ctr, info.Stream(), info.StreamSequence(), reply.Subject, info.TimeStamp().Format(time.RFC3339))
-			} else {
-				fmt.Printf("[#%d] Matched reply JetStream message: consumer: %s > %s / subject: %s / delivered: %d / consumer seq: %d / stream seq: %d\n", ctr, info.Stream(), info.Consumer(), reply.Subject, info.Delivered(), info.ConsumerSequence(), info.StreamSequence())
+			if reply != nil {
+				if info == nil {
+					fmt.Printf("[#%d]%s Matched reply on %q\n", ctr, timeStamp, reply.Subject)
+				} else if c.jetStream {
+					fmt.Printf("[#%d]%s Matched reply JetStream message: stream: %s seq %d / subject: %s / time: %v\n", ctr, timeStamp, info.Stream(), info.StreamSequence(), reply.Subject, info.TimeStamp().Format(time.RFC3339))
+				} else {
+					fmt.Printf("[#%d]%s Matched reply JetStream message: consumer: %s > %s / subject: %s / delivered: %d / consumer seq: %d / stream seq: %d\n", ctr, timeStamp, info.Stream(), info.Consumer(), reply.Subject, info.Delivered(), info.ConsumerSequence(), info.StreamSequence())
+				}
+
+				prettyPrintMsg(reply, c.headersOnly, c.translate)
+
 			}
-
-			prettyPrintMsg(reply, c.headersOnly, c.translate)
-
 		}
 	} // output format type dispatch
 }
