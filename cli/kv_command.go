b3d528db111b1745f6bebff0475b54381071cd2f
R.I.Pienaar <rip@devco.net>
2024-06-18 17:55:23 +0300

    Cap KV max value size to MaxInt32
    Signed-off-by: R.I.Pienaar <rip@devco.net>

--- cli/kv_command.go
+++ cli/kv_command.go
@@ -23,6 +23,8 @@ import (
 	"strings"
 	"time"
 
+	"github.com/nats-io/natscli/internal/util"
+
 	"github.com/AlecAivazis/survey/v2"
 	"github.com/choria-io/fisk"
 	"github.com/dustin/go-humanize"
@@ -165,6 +167,9 @@ func (c *kvCommand) parseLimitStrings(_ *fisk.ParseContext) (err error) {
 		c.maxValueSize, err = parseStringAsBytes(c.maxValueSizeString)
 		if err != nil {
 			return err
+		}
+		if c.maxValueSize > math.MaxInt32 {
+			return fmt.Errorf("max value size %s is too big maximum is %s", f(c.maxValueSize), f(math.MaxInt32))
 		}
 	}
 
