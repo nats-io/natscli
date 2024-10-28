0d162c2a8ab1db53711aa34a3cf097f3f164c4e5
joeriddles <joeriddles10@gmail.com>
2024-10-28 14:04:02 -0700

    Add TestEditFile to test CLI util editFile

--- cli/util_test.go
+++ cli/util_test.go
@@ -15,7 +15,10 @@ package cli
 
 import (
 	"errors"
+	"io"
+	"os"
 	"slices"
+	"strings"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -114,8 +117,56 @@ func TestSplitCommand(t *testing.T) {
 	// EOF found when expecting closing quote
 	_, _, err = splitCommand("foo --bar 'hello")
 	if err == nil {
-		t.Fatalf("Expdected err to not be nil, got %v", err)
+		t.Fatal("Expected err to not be nil, got nil")
 	}
+}
+
+func TestEditFile(t *testing.T) {
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+	defer r.Close()
+	defer w.Close()
+
+	f, err := os.CreateTemp("", "test_edit_file")
+	if err != nil {
+		t.Fatalf("Expected err to be nil, got %v", err)
+	}
+	defer f.Close()
+	defer os.Remove(f.Name())
+
+	t.Run("EDITOR unset", func(t *testing.T) {
+		os.Unsetenv("EDITOR")
+		err := editFile("")
+		if err == nil {
+			t.Fatal("Expected err to not be nil, got nil")
+		}
+	})
+
+	t.Run("EDITOR set", func(t *testing.T) {
+		os.Setenv("EDITOR", "echo")
+		err := editFile(f.Name())
+		if err != nil {
+			t.Fatalf("Expected err to be nil, got %v", err)
+		}
+
+		w.Close()
+		stdout, err := io.ReadAll(r)
+		if err != nil {
+			t.Fatalf("Expected err to be nil, got %v", err)
+		}
+		r.Close()
+
+		actual := string(stdout)
+		lines := strings.Split(actual, "\n")
+
+		if len(lines) != 2 || lines[1] != "" {
+			t.Fatalf("Expeced one line of output, got %v", actual)
+		}
+
+		if !strings.Contains(lines[0], "test_edit_file") {
+			t.Fatalf("Expected echo output, got %v", actual)
+		}
+	})
 }
 
 func TestRandomString(t *testing.T) {
