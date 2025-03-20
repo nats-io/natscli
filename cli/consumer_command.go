50304872a4ae986474eed48362475a80cfbda793
Pieter Loubser <ploubser@gmail.com>
2025-03-20 11:04:48 +0000

    (#1309) Make consumer and stream add commands behave the same
    Here we move the connectAndSetup step to the top of the consumer createAction,
    causing it to fail early if a connection cannot be established.
    
    We also check if jetstream is enabled when calling the prepareHelper util func,
    allowing us to fail early in all cases.

--- cli/consumer_command.go
+++ cli/consumer_command.go
@@ -2056,6 +2056,7 @@ func (c *consumerCmd) validateCfg(cfg *api.ConsumerConfig) (bool, []byte, []stri
 }
 
 func (c *consumerCmd) createAction(pc *fisk.ParseContext) (err error) {
+	c.connectAndSetup(true, false)
 	cfg, err := c.prepareConfig()
 	if err != nil {
 		return err
@@ -2085,8 +2086,6 @@ func (c *consumerCmd) createAction(pc *fisk.ParseContext) (err error) {
 
 		return os.WriteFile(c.outFile, j, 0600)
 	}
-
-	c.connectAndSetup(true, false)
 
 	err = c.checkConfigLevel(cfg)
 	if err != nil {
